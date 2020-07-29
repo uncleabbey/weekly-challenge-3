5e955250efaac7e85da9375c71492afac3deeb94
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-07-29 16:13:06 +0100

    Add weekly challenges four

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function minCost (n, cost) {
+	var arr = cost[0]
+	for(var i = 1, length1 = cost.length; i < length1; i++){
+		var asa = cost[i];
+		for(var j = 0, length2 = asa.length; j < length2; j++){
+			if (arr[j] > asa[j]) {
+				arr[j] = asa[j]
+			}
+		}
+	}
+	var sum = 0;
+	for(var i = 0, length1 = arr.length; i < length1; i++){
+		sum += arr[i]
+	}
+	return sum
+}
+minCost(null,[ [1, 2, 3], [1, 2, 3], [3, 3, 1]])
